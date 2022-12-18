12e146588ad0884007a94e8c11ce1f430aef8c34
Omar López García <111783739+omarlopezgarcia@users.noreply.github.com>
2022-12-18 01:48:15 -0500

    asd

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/', (req, res) => {res.sendFile('/home/ubuntu/projects/H221S2_29_OmarLopez/index.html')});
+
+app.use(express.static(__dirname + '/'));
 
 app.listen(3000);
 console.log('Server on port 3000');
